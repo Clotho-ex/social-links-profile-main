aed85851b328503faa296a014f48057faa904276
Prometheus <yusufcanvar90@gmail.com>
2024-02-16 15:21:28 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("github").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.open("https://github.com/Clotho-ex");
+});
+
+document.getElementById("mentor").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.open("https://www.frontendmentor.io/profile/Clotho-ex");
+});
+
+document.getElementById("linkedIn").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.open("https://www.linkedin.com/in/clotho/");
+});
+
+document.getElementById("twitter").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.open("https://twitter.com/lachesis_");
+});
+
+document.getElementById("insta").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.open("about:blank");
+});
